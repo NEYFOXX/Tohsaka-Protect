b948ca3c6f9edd609c23f9cc3be81440f9bd4f49
NEYFOXX <92888341+NEYFOXX@users.noreply.github.com>
2022-01-28 22:00:38 +0100

    Config bot and database file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: 'OTM2Njc1MDgyNzEyMDE0ODU4.YfQopg.0OI6g9nal0m6Q3cbAOzRzXaBgJM',
+    prefix: '+',
+    owners: ['922106161153585174', '548028946097111045'],
+    database: {
+        name: 'opensourcebot',
+        password: 'neyfox15'
+    }
+}
